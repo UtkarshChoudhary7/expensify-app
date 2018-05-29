398c4aba9d3b2cc77e8acbf7774a6f926fdcab8d
Utkarsh <utkarshchoudhary30@gmail.com>
2018-05-29 16:29:34 +0530

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
+import moment from 'moment';
+import amFormat from './amFormat';
 
 const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>&#8377;{amFormat(amount)}
+        -
+       {moment(createdAt).format('MMMM Do, YYYY')}</p>
     <button onClick={() => {
         dispatch(removeExpense({ id }));
       }}>Remove</button>
---
+++ src/components/amFormat.js
@@ -0,0 +1,11 @@
+export default (amount) => {
+  amount = amount.toString();
+  let amount_decimal = amount.substring(amount.length-2);
+  let amount_end = amount.substring(amount.length-5, amount.length-2);
+  let amount_rem = amount.substring(0, amount.length-5);
+  if (amount_rem != '') {
+    amount_end = ','+amount_end;
+    amount_decimal = '.'+amount_decimal;
+  }
+  return amount_rem.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + amount_end + amount_decimal;
+};
